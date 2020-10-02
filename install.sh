32e0d9e0683ba179b7c65316e620d1cf60c65470
Gennaro Tedesco <gennarotedesco@gmail.com>
2020-10-02 03:05:24 +0200

    added gitconfig

--- install.sh
+++ install.sh
@@ -13,5 +13,11 @@ cp -f vimrc ~/.vimrc
 echo "copying ranger config file..."
 cp -f rc.conf ~/.config/ranger/rc.conf 
 
+echo "copying visidata config file ..."
+cp -f visidatarc ~/.visidatarc
+
+echo "copying git config file ..."
+cp -f gitconfig ~/.gitconfig
+
 echo "copying glow config file..."
 mkdir ~/.config/glowconfig/ && cp -f customglow.json ~/.config/glowconfig/customglow.json
