24fb513a32d13daf0da8e82945bcfc1fc81c2493
Gennaro Tedesco <gennaro@billie.io>
2022-09-22 10:30:57 +0200

    adapted notify messages to new style (my PR)

--- nvim/lua/functions.lua
+++ nvim/lua/functions.lua
@@ -1,19 +1,19 @@
 local notify = require("notify")
 
 local function count_matches()
-   local cur_word = vim.fn.expandcmd('<cword>')
-   local count = vim.api.nvim_exec('%s/'..cur_word..'//ng', true)
-   notify(' '..count, "info")
+	local cur_word = vim.fn.expandcmd("<cword>")
+	local count = vim.api.nvim_exec("%s/" .. cur_word .. "//ng", true)
+	notify(" " .. count, "info", { title = "search: " .. cur_word, render = "simple" })
 end
 
 local function copy_error()
-   local error_message = vim.fn.trim(vim.fn.execute('1messages'))
-   if error_message ~= "" then
-	  notify(error_message, "error", {title = "copied message", render = "default"})
-	  vim.cmd("let @*='"..error_message.."'")
-   else
-	  notify(' No new messages', "info")
-   end
+	local error_message = vim.fn.trim(vim.fn.execute("1messages"))
+	if error_message ~= "" then
+		notify(error_message, "error", { title = "message copied!", render = "simple" })
+		vim.cmd("let @*='" .. error_message .. "'")
+	else
+		notify(" No new messages", "info")
+	end
 end
 
-return {count_matches = count_matches, copy_error = copy_error}
+return { count_matches = count_matches, copy_error = copy_error }
