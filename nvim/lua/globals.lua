21b85be07ab69f532b9d06eab239aa0c0e4ec16d
Gennaro Tedesco <gennaro@billie.io>
2022-09-23 07:32:43 +0200

    added stylua pre-commit hooks

--- nvim/lua/globals.lua
+++ nvim/lua/globals.lua
@@ -1,4 +1,4 @@
 P = function(v)
-  print(vim.inspect(v))
-  return v
+	print(vim.inspect(v))
+	return v
 end
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -43,8 +43,8 @@ require("notify").setup({
 })
 
 -- lastplace
-require("nvim-lastplace").setup{
-	lastplace_ignore_buftype = {"quickfix", "nofile", "help"},
-	lastplace_ignore_filetype = {"gitcommit", "gitrebase", "svn", "hgcommit"},
-	lastplace_open_folds = true
-}
+require("nvim-lastplace").setup({
+	lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+	lastplace_ignore_filetype = { "gitcommit", "gitrebase", "svn", "hgcommit" },
+	lastplace_open_folds = true,
+})
