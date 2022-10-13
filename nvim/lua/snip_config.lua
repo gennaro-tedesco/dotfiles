4b1cdb59b55210b68c130c1546b08508d97c15c9
Gennaro Tedesco <gennaro@billie.io>
2022-10-13 15:00:53 +0200

    fixed luasnip keymappings

--- nvim/lua/snip_config.lua
+++ nvim/lua/snip_config.lua
@@ -21,9 +21,21 @@ end, { silent = true })
 
 vim.keymap.set({ "i", "s" }, "<C-j>", function()
 	if ls.expand_or_jumpable(-1) then
-		ls.expand_or_jump(-1)
+		ls.jump(-1)
 	end
 end, { silent = true })
+
+vim.keymap.set({ "i", "s" }, "<C-l>", function()
+	if ls.choice_active() then
+		ls.change_choice(1)
+	end
+end)
+
+vim.keymap.set({ "i", "s" }, "<C-h>", function()
+	if ls.choice_active() then
+		ls.change_choice(-1)
+	end
+end)
 
 -- snippets
 local snips = {
