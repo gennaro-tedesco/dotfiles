8cb101d143871881cd0256259329976b7ab10da4
Gennaro Tedesco <gennarotedesco@gmail.com>
2022-10-19 08:28:10 +0200

    changed hierarchy in completion sources

--- nvim/lua/cmp_config.lua
+++ nvim/lua/cmp_config.lua
@@ -58,11 +58,11 @@ cmp.setup({
 		end, { "i", "s" }),
 	},
 	sources = {
-		{ name = "nvim_lsp", keyword_length = 2 },
-		{ name = "nvim_lsp_signature_help" },
 		{ name = "luasnip", keyword_length = 2 },
-		{ name = "path" },
+		{ name = "nvim_lsp", keyword_length = 3 },
+		{ name = "nvim_lsp_signature_help" },
 		{ name = "buffer", keyword_length = 5 },
+		{ name = "path" },
 	},
 })
 
