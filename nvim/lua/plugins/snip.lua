bee02e4ab8a741cafb0a5a69f42b9463de7f9066
Gennaro Tedesco <gennaro@billie.io>
2022-11-10 15:39:41 +0100

    added list_snips as function and SnipList as command

--- nvim/lua/plugins/snip.lua
+++ nvim/lua/plugins/snip.lua
@@ -226,3 +226,14 @@ ls.add_snippets(nil, {
 	vim = snips.vim,
 	zsh = snips.zsh,
 })
+
+local list_snips = function()
+	local ft_list = require("luasnip").available()[vim.o.filetype]
+	local ft_snips = {}
+	for _, item in pairs(ft_list) do
+		ft_snips[item.trigger] = item.name
+	end
+	P(ft_snips)
+end
+
+vim.api.nvim_create_user_command("SnipList", list_snips, {})
