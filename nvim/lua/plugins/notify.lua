7ecdc8cae0b1d6c19edb2029fb9af7c54a678085
Gennaro Tedesco <gennaro@billie.io>
2022-11-10 16:34:16 +0100

    changed colour of notify info body text

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -3,6 +3,16 @@ if not ok then
 	return
 end
 
+local notify_hl = vim.api.nvim_create_augroup("NotifyHighlights", {})
+vim.api.nvim_clear_autocmds({ group = notify_hl })
+vim.api.nvim_create_autocmd("BufEnter", {
+	group = notify_hl,
+	callback = function()
+		vim.api.nvim_set_hl(0, "NotifyINFOIcon", {})
+		vim.api.nvim_set_hl(0, "NotifyINFOIcon", { link = "Character" })
+	end,
+})
+
 notify.setup({
 	timeout = 5000,
 	render = "minimal",
