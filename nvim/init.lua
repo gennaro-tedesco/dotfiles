9b1325457f91338ceb1d00a940a64e4dcc8cb9b1
Gennaro Tedesco <gennaro@billie.io>
2022-12-24 03:05:19 +0100

    added lsp and cmp

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,10 +7,9 @@ vim.opt.termguicolors = true
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-----------------------------
---- plugins declarations ---
-----------------------------
-
+---------------------------------
+--- plugins manager:lazy.nvim ---
+---------------------------------
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
@@ -24,6 +23,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.runtimepath:prepend(lazypath)
 
+--------------------
+--- plugins list ---
+--------------------
 local plugins = {
 	--- colorschemes, looks and syntax highlights
 	{
@@ -52,6 +54,52 @@ local plugins = {
 				mappings = { "<C-u>", "<C-d>", "zt", "zz", "zb" },
 			})
 		end,
+	},
+	{
+		"folke/noice.nvim",
+		dependencies = {
+			{ "MunifTanjim/nui.nvim" },
+			{
+				"rcarriga/nvim-notify",
+				config = function()
+					require("plugins.notify")
+				end,
+			},
+		},
+		config = function()
+			require("plugins.noice")
+		end,
+	},
+	--- LSP
+	{
+		"neovim/nvim-lspconfig",
+		event = "BufReadPre",
+		config = function()
+			require("plugins.lsp")
+		end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			{
+				"windwp/nvim-autopairs",
+				config = function()
+					require("plugins.autopairs")
+				end,
+			},
+		},
+		config = function()
+			require("plugins.cmp")
+		end,
+	},
+	{
+		"smjonas/inc-rename.nvim",
+		config = true,
 	},
 }
 require("lazy").setup(plugins)
---
+++ nvim/lua/plugins/notify.lua
@@ -0,0 +1,25 @@
+local ok, notify = pcall(require, "notify")
+if not ok then
+	return
+end
+
+local notify_hl = vim.api.nvim_create_augroup("NotifyHighlights", {})
+vim.api.nvim_clear_autocmds({ group = notify_hl })
+vim.api.nvim_create_autocmd("BufEnter", {
+	group = notify_hl,
+	callback = function()
+		vim.api.nvim_set_hl(0, "NotifyINFOIcon", {})
+		vim.api.nvim_set_hl(0, "NotifyINFOIcon", { link = "Character" })
+	end,
+})
+
+notify.setup({
+	timeout = 5000,
+	render = "minimal",
+	stages = "fade_in_slide_out",
+})
+
+vim.keymap.set("n", "<Esc>", function()
+	require("notify").dismiss()
+	vim.cmd.nohlsearch()
+end)
