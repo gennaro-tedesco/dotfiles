7c6ab3b27c7f69575158365b488c2c30ff3fd607
Gennaro Tedesco <gennaro@billie.io>
2022-12-24 19:54:47 +0100

    removed ft loading for vimtex

--- nvim/init.lua
+++ nvim/init.lua
@@ -146,7 +146,6 @@ local plugins = {
 	},
 	{
 		"lervag/vimtex",
-		ft = "tex",
 		init = function()
 			vim.g.vimtex_view_general_viewer = "zathura"
 			vim.g.vimtex_view_zathura_options = "-reuse-instance"
