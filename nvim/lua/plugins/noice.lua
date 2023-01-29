cfe31162500efd053ab6ba89630a555acef35c2b
Gennaro Tedesco <gennaro@billie.io>
2023-01-29 21:21:36 +0100

    added insert modes in noice scrolling for docs and signature window

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -117,13 +117,13 @@ noice.setup({
 	},
 })
 
-vim.keymap.set("n", "<c-j>", function()
+vim.keymap.set({ "n", "i", "s" }, "<c-j>", function()
 	if not require("noice.lsp").scroll(4) then
 		return "<c-j>"
 	end
 end, { silent = true, expr = true })
 
-vim.keymap.set("n", "<c-k>", function()
+vim.keymap.set({ "n", "i", "s" }, "<c-k>", function()
 	if not require("noice.lsp").scroll(-4) then
 		return "<c-k>"
 	end
