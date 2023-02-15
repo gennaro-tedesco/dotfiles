c23bdebc03746e08fee5444a7802d0a97baf608f
Gennaro Tedesco <gennaro@billie.io>
2023-02-15 15:16:09 +0100

    improved dockerfile detection in filetype.add

--- nvim/lua/filetype.lua
+++ nvim/lua/filetype.lua
@@ -2,11 +2,11 @@ vim.filetype.add({
 	filename = {
 		[".env"] = "config",
 		[".todo"] = "txt",
-		[".dockerignore"] = "gitignore",
 	},
 	pattern = {
 		["req.*.txt"] = "config",
 		["env.*"] = "config",
 		["gitconf.*"] = "gitconfig",
+		[".*/%.dockerignore"] = "gitignore",
 	},
 })
