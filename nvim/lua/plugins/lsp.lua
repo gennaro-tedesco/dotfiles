fffb13d69593480c9780973ba49cf14c44ce9219
Gennaro Tedesco <gennaro@billie.io>
2023-03-15 14:07:26 +0100

    improved wildcards for jsonschemas lsp and added argument for preview_files for todo

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -199,8 +199,8 @@ lsp.yamlls.setup({
 			schemas = {
 				["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
 				["https://json.schemastore.org/pre-commit-config.json"] = "/.pre-commit-config.*",
-				["https://raw.githubusercontent.com/iterative/dvcyaml-schema/master/schema.json"] = "dvc.yaml",
-				["https://json.schemastore.org/swagger-2.0.json"] = "swagger.yaml",
+				["https://raw.githubusercontent.com/iterative/dvcyaml-schema/master/schema.json"] = "**/dvc.yaml",
+				["https://json.schemastore.org/swagger-2.0.json"] = "**/swagger.yaml",
 			},
 		},
 	},
