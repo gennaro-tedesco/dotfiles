bc9acae08a4104ff5f1adbe8eef3ee58e11e1020
Gennaro Tedesco <gennaro@billie.io>
2023-04-13 13:37:46 +0200

    removed plug_list function

--- nvim/init.lua
+++ nvim/init.lua
@@ -411,34 +411,6 @@ for _, file in ipairs(vim.fn.readdir(config_path .. "/lua", [[v:val =~ '\.lua$']
 	require(file:gsub("%.lua$", ""))
 end
 
-local function plug_list()
-	local plugins_file = config_path .. "/init.lua"
-
-	local loc_list = {}
-	for _, p in pairs(lazy.plugins()) do
-		if p[1] ~= nil then
-			local plugin_pattern = '"' .. p[1]:gsub("/", "\\/") .. '"'
-			local row = vim.fn.search(plugin_pattern)
-			local col =
-				tonumber(vim.api.nvim_exec("g/" .. plugin_pattern .. '/execute "normal! ^" | echo col(".")-1', true))
-			if row ~= 0 then
-				table.insert(loc_list, {
-					filename = plugins_file,
-					lnum = row,
-					col = col + 1,
-					text = p[1],
-				})
-			end
-		end
-	end
-	table.sort(loc_list, function(a, b)
-		return string.upper(a.text:gsub(".*/", "")) < string.upper(b.text:gsub(".*/", ""))
-	end)
-	vim.fn.setloclist(0, loc_list, " ")
-	vim.cmd("lopen")
-end
-vim.api.nvim_create_user_command("PlugList", plug_list, {})
-
 local function install()
 	local mk_path, mk_cmd = vim.fs.normalize("~/dotfiles"), "install-nvim"
 	vim.cmd("!make -C " .. mk_path .. " " .. mk_cmd)
