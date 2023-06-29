55005ff3c01e9da62e8fa4775f36b44706b55d8a
Gennaro Tedesco <gennaro@billie.io>
2023-06-29 13:01:53 +0200

    fixed highlight groups for flash

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -4,17 +4,17 @@ if not ok then
 end
 
 flash.setup({
-	highlight = {
-		label = { style = "eol" },
-		groups = { backdrop = "SignColumn" },
-	},
+	label = { min_pattern_length = 2, style = "eol" },
+	highlight = { groups = { backdrop = "SignColumn" } },
 	modes = {
-		char = { highlight = { backdrop = false } },
+		char = { highlight = { backdrop = false }, search = { wrap = true } },
 		search = { highlight = { groups = { label = "Todo" } } },
 		treesitter = {
-			highlight = {
-				groups = { label = "Todo" },
-				label = { before = true, after = true, style = "overlay" },
+			label = {
+				before = true,
+				after = true,
+				style = "overlay",
+				rainbow = { enabled = true, shade = 5 },
 			},
 		},
 	},
@@ -34,5 +34,6 @@ vim.keymap.set({ "n", "x", "o" }, "<leader>t", function()
 				vim.fn.winrestview(view)
 			end)
 		end,
+		highlight = { backdrop = false },
 	})
 end, { desc = "flash treesitter operator" })
