2d151bfa83bdc6f628cc96ce585c0349ffee27c8
Gennaro Tedesco <g.tedesco@finlink.de>
2023-11-04 11:16:21 +0100

    added lazyloading event for vim-matchup

--- nvim/init.lua
+++ nvim/init.lua
@@ -450,6 +450,7 @@ local plugins = {
 	},
 	{
 		"andymass/vim-matchup",
+		event = "BufReadPost",
 		init = function()
 			vim.g.matchup_matchparen_offscreen = { method = "popup" }
 			vim.g.matchup_surround_enabled = 1
