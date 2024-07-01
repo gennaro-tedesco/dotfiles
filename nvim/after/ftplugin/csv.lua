f109c209959b1fba470917e3b9b8599ea0b141f1
Gennaro Tedesco <gennarotedesco@gmail.com>
2024-07-02 00:09:20 +0200

    fixed description in csv plugin

--- nvim/after/ftplugin/csv.lua
+++ nvim/after/ftplugin/csv.lua
@@ -1,2 +1,2 @@
-vim.keymap.set("n", "<Right>", "f,l", { buffer = 0, desc = "close git commit view" })
-vim.keymap.set("n", "<Left>", "F,b", { buffer = 0, desc = "close git commit view" })
+vim.keymap.set("n", "<Right>", "f,l", { buffer = 0, desc = "csv go to next field" })
+vim.keymap.set("n", "<Left>", "F,b", { buffer = 0, desc = "csv go to previous field" })
