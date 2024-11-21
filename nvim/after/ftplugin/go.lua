f62fbc35fb079b529015a808a8d5ed3531a362b6
Gennaro Tedesco <g.tedesco@finlink.de>
2024-11-21 19:47:36 +0100

    improved go.lua specific filetype functions to accommodate for go build

--- nvim/after/ftplugin/go.lua
+++ nvim/after/ftplugin/go.lua
@@ -1,6 +1,16 @@
-vim.api.nvim_create_user_command("GoModTidy", function()
+vim.cmd.compiler("go")
+
+vim.api.nvim_create_user_command("Build", function()
+	vim.cmd.make({ args = { "." }, mods = { silent = true } })
+end, {})
+
+vim.api.nvim_create_user_command("Run", function()
+	require("FTerm").run({ "go", "run", "." })
+end, {})
+
+vim.api.nvim_create_user_command("Tidy", function()
 	vim.cmd.LspStop()
-	vim.cmd("!go mod tidy -v")
+	vim.system({ "go", "mod", "tidy", "-v" }, { text = true })
 	vim.cmd.write()
 	vim.cmd.LspStart()
 end, {})
