31a48a34a2ab30a7959fd457ba16026ff796ae3f
Gennaro Tedesco <g.tedesco@finlink.de>
2025-06-02 11:56:42 +0200

    config: (nvim) add copilot setup

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,53 @@
+local ok, chat = pcall(require, "CopilotChat")
+if not ok then
+	return
+end
+
+local icons = require("utils").icons
+
+local concise_prompt = [[
+	1. be concise and to the point
+	2. only answer the question asked
+	3. when possible, provide references to documentation
+	4. only give links if they exist and are not 404
+	]]
+
+chat.setup({
+	model = "claude-3.7-sonnet",
+	system_prompt = concise_prompt,
+	question_header = "# ",
+	answer_header = "# 🤖 🤖",
+	error_header = "### " .. icons.statusline.Error,
+	highlight_headers = false,
+	separator = "",
+	mappings = {
+		accept_diff = {
+			normal = "gp",
+			insert = "gp",
+		},
+		show_diff = {
+			full_diff = true,
+		},
+		show_help = { normal = "g?" },
+		stop = {
+			normal = "<C-c>",
+			callback = function()
+				require("CopilotChat").stop()
+			end,
+		},
+	},
+	providers = {
+		github_models = {
+			disabled = true,
+		},
+	},
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	pattern = "copilot-*",
+	callback = function()
+		vim.opt_local.relativenumber = false
+		vim.opt_local.number = false
+		vim.opt_local.conceallevel = 0
+	end,
+})
